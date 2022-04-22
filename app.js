09c3e0cc0f520eaa630900c1598381071a5ace86
JaviMx <javimxmine@hotmail.com>
2022-04-22 17:35:08 -0500

    request POST

--- app.js
+++ app.js
@@ -28,3 +28,10 @@ app.get('/v1/explorers/:id', (req, res) =>{
     const explorer = {id: 1, name: "Javi"}
     res.status(200).json(explorer)
 })
+
+
+app.post('/v1/explorers', (req, res) =>{
+    console.log(`Api Explorers POST request ${new Date()}`)
+    const requestBody = req.body
+    res.status(201).json({message: "created"})
+})
